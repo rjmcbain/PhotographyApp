0ea747dd16c6b165958c5b39308c5e60dcf55f37
rjmcbain <rmcbain01@gmail.com>
2018-02-23 15:19:49 -0700

    finished auth

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,26 @@
 const express = require ('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const port = process.env.PORT || 3000;
+
 require('./models/User');
 require('./services/passport');
+const keys = require('./config/keys');
 
 
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/photography" );
 
 const app = express();
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
