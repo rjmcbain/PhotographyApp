0ae55e5216eaf7d95e0ed06b50f45378c4db4af3
rjmcbain <rmcbain01@gmail.com>
2018-02-23 13:23:30 -0700

    adding backend

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,21 @@
 const express = require ('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 const port = process.env.PORT || 3000;
+require('./models/User');
+require('./services/passport');
 
+
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/photography" );
 
 const app = express();
+
+authRoutes(app);
 
 app.get('/',(req, res) => {
     res.send("Hello World")
 });
+
 
 
 
