bcdcbf15eae02bcf6c06bebe94d43fcd179882f7
rjmcbain <rmcbain01@gmail.com>
2018-02-24 18:16:29 -0700

    adding gitignore

---
+++ back-end/config/dev.js
@@ -0,0 +1,6 @@
+//dev.js - dont commit this
+module.exports = {
+    googleClientID: '242046370864-sqb6glang49034mf4svtcu3a3mlrc0do.apps.googleusercontent.com',
+    googleClientSecret: 'A8h_UY1f0zmlmKqTBui5qF0I',
+    cookieKey: 'yoMomma'
+};
