f1938fa34e126eda18130e03f218072b203c35f4
rjmcbain <rmcbain01@gmail.com>
2018-02-26 16:50:51 -0700

    fixed lint issue

--- back-end/functions/index.js
+++ back-end/functions/index.js
@@ -1,3 +1,10 @@
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
 const functions = require('firebase-functions');
 const gcs = require('@google-cloud/storage')();
 const os = require('os');
